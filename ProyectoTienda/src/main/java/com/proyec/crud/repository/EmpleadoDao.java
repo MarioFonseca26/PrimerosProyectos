aade8085fcc2ac8026d689b12019310be7495e39
MarioFonseca26 <110215753+MarioFonseca26@users.noreply.github.com>
2022-07-29 15:39:24 -0600

    Delete ProyectoTienda directory

--- ProyectoTienda/src/main/java/com/proyec/crud/repository/EmpleadoDao.java
+++
@@ -1,8 +0,0 @@
-
-package com.proyec.crud.repository;
-import com.proyec.crud.domain.Empleado;
-import org.springframework.data.repository.CrudRepository;
-
-public interface EmpleadoDao extends CrudRepository<Empleado, Long>{
-    
-}
