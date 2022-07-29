d43db9fd82d0b8233c1305fb5711d9525ef20462
MarioFonseca26 <110215753+MarioFonseca26@users.noreply.github.com>
2022-07-29 15:17:31 -0600

    Delete CrudFacturacion directory

--- CrudFacturacion/src/main/java/com/example/crudfactura/mario/repository/EmpleadoDao.java
+++
@@ -1,9 +0,0 @@
-package com.example.crudfactura.mario.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.example.crudfactura.mario.model.Empleado;
-
-public interface EmpleadoDao extends CrudRepository<Empleado, Long> {
-    
-}
